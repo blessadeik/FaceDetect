eba992e6a369b22a63dbdea6129981f53d1fda06
ADEIKA, Blessing Isoyiza <blessadeik@gmail.com>
2021-10-08 16:14:53 +0100

    facedetect

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, {Component}from 'React';
+import Navigation from './components/Navigation/Navigation';
+import './App.css';
+
+class App extends Component {
+  render() {
+      return (
+      <div className="App">
+        <Navigation />
+        {/*<Logo/>
+        <ImageLinkForm />
+        <FaceRecognition />*/}
+      </div>
+    );
+  }
+  
+}
+
+export default App;
